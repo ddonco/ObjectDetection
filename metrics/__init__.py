8518abd54adbc35f59360736a47101ed9159c60a
ddonco <dillon.donohue@gates.com>
2019-08-19 16:45:33 -0600

    Refactor: make metrics drectory and add metrics files

---
+++ metrics/__init__.py
@@ -0,0 +1,7 @@
+###########################################################################################
+# Developed by: Rafael Padilla (rafael.padilla@smt.ufrj.br)                               #
+#        https://github.com/rafaelpadilla/Object-Detection-Metrics                        #
+#        SMT - Signal Multimedia and Telecommunications Lab                               #
+#        COPPE - Universidade Federal do Rio de Janeiro                                   #
+#        Last modification: Oct 9th 2018                                                  #
+###########################################################################################
