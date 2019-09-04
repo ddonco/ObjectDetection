bc22269bab94ea0b042374f950bc52a9552a76d1
ddonco <dillon.donohue@gates.com>
2019-09-04 15:56:49 -0600

    debug: troubleshooting focal loss

--- helper/object_detection_helper.py
+++ helper/object_detection_helper.py
@@ -72,7 +72,9 @@ def show_boxes(boxes):
 def activ_to_bbox(acts, anchors, flatten=True):
     "Extrapolate bounding boxes on anchors from the model activations."
     if flatten:
+        print(f"acts: {acts.size()}")
         acts.mul_(acts.new_tensor([[0.1, 0.1, 0.2, 0.2]]))
+        print(f"acts: {acts.size()}, anchors: {anchors.size()}")
         centers = anchors[...,2:] * acts[...,:2] + anchors[...,:2]
         sizes = anchors[...,2:] * torch.exp(acts[...,2:])
         return torch.cat([centers, sizes], -1)
