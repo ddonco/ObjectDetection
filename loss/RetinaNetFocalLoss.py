937671e7109e35e3b0984db1decbdd76cc6d16ff
ddonco <dillon.donohue@gates.com>
2019-09-04 13:48:54 -0600

    feat: resize class preditions tensor in Focal Loss to match anchor tensor length

--- loss/RetinaNetFocalLoss.py
+++ loss/RetinaNetFocalLoss.py
@@ -40,7 +40,9 @@ class RetinaNetFocalLoss(nn.Module):
         matches.add_(1)
         clas_tgt = clas_tgt + 1
         clas_mask = matches >= 0
-        clas_pred = clas_pred[clas_mask]
+        clas_pred_resize = torch.zeros(clas_mask.size(0), clas_pred.size(1))
+        clas_pred_resize[:clas_pred.size(0), :] = clas_pred
+        clas_pred = clas_pred_resize[clas_mask]
         clas_tgt = torch.cat([clas_tgt.new_zeros(1).long(), clas_tgt])
         clas_tgt = clas_tgt[matches[clas_mask]]
         return bb_loss, self._focal_loss(clas_pred, clas_tgt) / torch.clamp(bbox_mask.sum(), min=1.)
